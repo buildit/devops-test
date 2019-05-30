b6a64e2d3b145d4516711bc140a1d76f30d86b2c
Joel Calliste <joel.calliste@ibm.com>
2019-05-30 14:52:25 +0100

    updated index.js output

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var os = require('os')
 var http = require('http')
 
 function handleRequest(req, res) {
-  res.write('Hi there! I\'m being served from ' + os.hostname() + ' using pm2. Deployed by Codeship')
+  res.write('Hi there! I\'m being served from ' + os.hostname() + ' using pm2. Built, tested and deployed by Codeship')
   res.end()
 }
 
