cfdec98d9fcc37ccc1ca19c84e31572ca72b6632
vibhor995 <vibhor.kumar@thoughtspot.com>
2020-07-18 21:16:27 +0530

    testing

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+'use strict';
+
+const express = require('express');
+
 var os = require('os')
-var http = require('http')
 
-function handleRequest(req, res) {
-  res.write('Hi there! I\'m being served from ' + os.hostname())
-  res.end()
-}
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
-http.createServer(handleRequest).listen(3000)
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hi there! I\'m being served from ' + os.hostname());
+});
+
+app.listen(PORT, HOST);
