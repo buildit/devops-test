ffe2720325278903d40b44334d0f0d6068d5cb02
michael <leyshon@gmail.com>
2020-09-28 21:22:24 +0100

    Adding version to the setup script

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt update
 sudo apt install npm -y
-curl https://buildittest.s3-eu-west-1.amazonaws.com/test-app.tar.gz -o test-app.tar.gz
+curl https://buildittest.s3-eu-west-1.amazonaws.com/test-app-${version}.tar.gz -o test-app.tar.gz
 tar -xvf test-app.tar.gz
 cd test-app
 npm start
