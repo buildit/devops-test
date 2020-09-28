b7e20f68f73819b5bb9f6c22635a4a45a17e6c95
michael <leyshon@gmail.com>
2020-09-28 21:05:53 +0100

    Fixing the name of the ssh key

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "buildit" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public[count.index].id
-  key_name               = aws_key_pair.michael_laptop.key_name
+  key_name               = aws_key_pair.public.key_name
   vpc_security_group_ids = [aws_security_group.allow_3000.id]
     user_data = templatefile("setup.sh", {
     version = var.app_version
