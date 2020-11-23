4be67da8de467ddeaa0a72c533b826165f16a1f9
Viktor Radnai <viktor.radnai@gmail.com>
2020-11-23 18:42:17 +0000

    terraform: store state in GCS bucket

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -2,3 +2,10 @@ provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "vik-wipro-terraform-state"
+    prefix = "devops-test"
+  }
+}
