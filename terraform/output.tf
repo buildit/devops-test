cc351f85c85f1e23c31fb17597937466c4a1cd0e
Viktor Radnai <viktor.radnai@gmail.com>
2020-11-23 22:46:45 +0000

    Format output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,18 +1,4 @@
-output "instance_group_" {
-  value = module.managed_instance_group.instance_group
-}
-
-output "instance_group_manager" {
-  value = module.managed_instance_group.instance_group_manager
-}
-
-
-output "backend_services" {
-  description = "The backend service resources."
-  value       = module.lb-http.backend_services
-}
-
-output "external_ip" {
-  description = "The external IP assigned to the global fowarding rule."
-  value       = module.lb-http.external_ip
+output "external_url" {
+  description = "The full URL for reaching the load balancer."
+  value       = "http://${module.lb-http.external_ip}/"
 }
