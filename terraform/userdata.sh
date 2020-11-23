1340aec66dc1b5921e469c49490b1e81fe5133b4
Viktor Radnai <viktor.radnai@gmail.com>
2020-11-23 11:04:08 +0000

    Download and run the wipro devops-test service

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -xe
 
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
+apt-get install nodejs npm git netcat -y
 
-curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+node -v
 
-# Replace with the branch of Node.js or io.js you want to install: node_6.x, node_8.x, etc...
-VERSION=node_8.x
-# The below command will set this correctly, but if lsb_release isn't available, you can set it manually:
-# - For Debian distributions: jessie, sid, etc...
-# - For Ubuntu distributions: xenial, bionic, etc...
-# - For Debian or Ubuntu derived distributions your best option is to use the codename corresponding to the upstream release your distribution is based off. This is an advanced scenario and unsupported if your distribution is not listed as supported per earlier in this README.
-DISTRO="$(lsb_release -s -c)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+git clone https://github.com/viktorradnai/devops-test
+cd devops-test
+npm start & while ! nc -z localhost 3000; do
+    sleep 1
+done
+curl -sf http://localhost:3000
+echo
