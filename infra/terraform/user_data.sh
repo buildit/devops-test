f4df60bceafc2200d8609cb9fabcb7677cf16e8b
Ravi <ongole.sr@gmail.com>
2021-03-01 23:46:43 +0000

    vpc worksgit status

--- infra/terraform/user_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-
-# Config ECS agent
-echo "ECS_CLUSTER=${ecs_cluster}" > /etc/ecs/ecs.config
--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -6,10 +6,10 @@ module "vpc" {
   version = ">= 2.70.0"
 
   name               = "demo-dev"
-  cidr               = "10.0.0.0/28"
-  azs                = ["eu-west-2a","eu-west-2b"]
-  private_subnets    = ["10.0.0.0/29", "10.0.0.8/29"]
-  public_subnets     = ["10.0.0.16/29", "10.0.0.24/29"]
+  cidr               = "10.0.0.0/24"
+  azs                = ["eu-west-2a"]
+  private_subnets    = ["10.0.0.0/28"]
+  public_subnets     = ["10.0.0.224/28"]
   enable_nat_gateway = true
   single_nat_gateway = true
   tags               = {
