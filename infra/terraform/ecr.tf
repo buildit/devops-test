477445f0d31e641952fb11b1b247d5295e9b65b3
Ravi <ongole.sr@gmail.com>
2021-03-02 01:25:30 +0000

    ECR created and it worksgit add ecr.tf

---
+++ infra/terraform/ecr.tf
@@ -0,0 +1,31 @@
+###
+# Repo
+###
+resource "aws_ecr_repository" "ecr" {
+  name = "demo-ecr"
+}
+###
+# Purge images older than 5 days
+###
+resource "aws_ecr_lifecycle_policy" "ecr_policy" {
+  repository = aws_ecr_repository.ecr.name
+  policy = <<EOF
+    {
+        "rules": [
+            {
+                "rulePriority": 1,
+                "description": "Purge images older than 5 days",
+                "selection": {
+                    "tagStatus": "any",
+                    "countType": "sinceImagePushed",
+                    "countUnit": "days",
+                    "countNumber": 5
+                },
+                "action": {
+                    "type": "expire"
+                }
+            }
+        ]
+    }
+    EOF
+}
